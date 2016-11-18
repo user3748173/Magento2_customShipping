1bc586d240061c081f436fa9c43b4e3d1106ea4a
user3748173 <user3748173@users.noreply.github.com>
2016-11-18 14:20:58 +0200

    Adding working ajax return

--- checkoutShipping/Controller/Index/Htmlcreator.php
+++ checkoutShipping/Controller/Index/Htmlcreator.php
@@ -4,17 +4,21 @@ namespace Vendor\checkoutShipping\Controller\Index;
 
 class Htmlcreator extends \Magento\Framework\App\Action\Action{
 
-    public function __construct(
-    \Magento\Framework\Model\Context $context, \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory) {
+    public function __construct( 
+        \Vendor\checkoutShipping\Model\getCarrierForHtml $getCarrierForHtml,
+        \Magento\Framework\Model\Context $context, 
+        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory) {
         $this->resultJsonFactory = $resultJsonFactory;
         parent::__construct($context);
+        $this->_getCarrierForHtml = $getCarrierForHtml;
     }
 
     public function execute() {
         $result = $this->resultJsonFactory->create();
         if ($this->getRequest()->isAjax()) {
-            $test = '<h1>Tere</h1>';
-            return $result->setData($test);
+            $test = $this->getRequest()->getPost("ajax");
+            $htmlData=$this->_getCarrierForHtml->getSetingsFromDb($test);
+            return $result->setData($htmlData);
         }
     }
 
