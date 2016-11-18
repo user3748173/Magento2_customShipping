2109d5ba9aeb59a88219d6b0eabba418272be1c2
user3748173 <user3748173@users.noreply.github.com>
2016-11-18 14:36:45 +0200

    Display dropdown in front-end

--- checkoutShipping/view/frontend/web/js/view/processShippingMethod.js
+++ checkoutShipping/view/frontend/web/js/view/processShippingMethod.js
@@ -7,10 +7,8 @@ define([
             if (shippingMethod !== null) {
                 var value = shippingMethod.carrier_code;
                 var divId = "carrier_" + value;
-                if (!jQuery(divId).length) {
-                   var HtmlCode=ajaxCall(value);
-                   if(HtmlCode){
-                    console.log(HtmlCode);
+                if (!jQuery(divId).length && !jQuery.active)  {
+                    ajaxCall(value);
                 }
                 } else {
                     return true;
@@ -21,16 +19,19 @@ define([
     };
     function ajaxCall(shippingMethod)
     {
-        console.log(shippingMethod);
+        var param = 'ajax=' + shippingMethod;
         jQuery.ajax({
             type: 'post',
-            url: 'https://127.0.0.1:8443/vendor_module/index/htmlcreator',
-            data: {'selectedShippingMehtod': shippingMethod},
+            url: '/vendor_shipping/index/htmlcreator',
+            data: param,
             dataType: 'json'
         }).done(function (msg) {
-            var variable = "Data Saved: " + msg;
-            return variable;
+            var variable = msg;
+            htmlOut(shippingMethod, variable);
         });
+    };
+    function htmlOut(shippingMethod, html) {        
+        jQuery( ".carrier_settings" ).remove();
+        jQuery('#onepage-checkout-shipping-method-additional-load').before("<div id='carrier_"+shippingMethod+"' class='carrier_settings'><div class='step-title' data-bind='i18n: 'Shipping Carrier'' data-role='title'>Shipping Carrier</div>"+html+"</br></br></div>");
     }
-    ;
 });
