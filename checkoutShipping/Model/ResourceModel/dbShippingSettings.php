1bc2734e3b1c9f383a0dbc897d1ea517f999453f
user3748173 <user3748173@users.noreply.github.com>
2016-12-31 21:26:56 +0200

    removed unnecessary function

--- checkoutShipping/Model/ResourceModel/dbShippingSettings.php
+++ checkoutShipping/Model/ResourceModel/dbShippingSettings.php
@@ -3,9 +3,6 @@ namespace Vendor\checkoutShipping\Model\ResourceModel;
 
 class dbShippingSettings extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 {
-public function __construct(\Vendor\checkoutShipping\Model\getDbTable $getDbTable) {
-        $this->_getDbTable = $getDbTable;
-}
 public function _construct() {
         $this->_init('custom_shipping_settings', 'template_id');
 }
