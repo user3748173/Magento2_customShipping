44c7c7e486ef29e9f0d148aad7cf45e768eb5049
user3748173 <user3748173@users.noreply.github.com>
2017-01-07 01:43:07 +0200

    added setSearchElement function

--- checkoutShipping/view/frontend/web/js/view/checkout/shipping/additional-block.js
+++ checkoutShipping/view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -4,15 +4,18 @@ define([
     'use strict';
     var show_hide_custom_blockConfig = window.checkoutConfig.show_hide_custom_block;
     return Component.extend({
+        setSearchElement: function (element) {
+            console.log(element);
             var availableTags = [
                 "ActionScript",
                 "AppleScript",
                 "Asp"
             ];
-            jQuery(element).autocomplete({
+            jQuery('#tags').autocomplete({
                 source: availableTags,
                 minLength: 2
-            });,
+            });
+        },
         canVisibleBlock: show_hide_custom_blockConfig
     });
 });
