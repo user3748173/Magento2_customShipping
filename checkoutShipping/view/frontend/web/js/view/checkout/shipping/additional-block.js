02f7cc0b7431b4cbac780808c597c2b403e26c57
user3748173 <user3748173@users.noreply.github.com>
2017-01-07 01:12:07 +0200

    Added autocomplete

--- checkoutShipping/view/frontend/web/js/view/checkout/shipping/additional-block.js
+++ checkoutShipping/view/frontend/web/js/view/checkout/shipping/additional-block.js
@@ -4,9 +4,15 @@ define([
     'use strict';
     var show_hide_custom_blockConfig = window.checkoutConfig.show_hide_custom_block;
     return Component.extend({
-        defaults: {
-            template: 'Vendor_checkoutShipping/checkout/shipping/additional-block'
-        },
+            var availableTags = [
+                "ActionScript",
+                "AppleScript",
+                "Asp"
+            ];
+            jQuery(element).autocomplete({
+                source: availableTags,
+                minLength: 2
+            });,
         canVisibleBlock: show_hide_custom_blockConfig
     });
 });
